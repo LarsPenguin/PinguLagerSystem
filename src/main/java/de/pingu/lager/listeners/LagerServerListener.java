2213c2bb46d5cffe3c61c9b262bfc3bc415fe8fc
LarsPenguin <grizzlybln123@gmail.com>
2025-12-06 23:16:33 +0100

    Add null checks for item in hand
    Add null checks for item in hand in onPlace method.

--- src/main/java/de/pingu/lager/listeners/LagerServerListener.java
+++ src/main/java/de/pingu/lager/listeners/LagerServerListener.java
@@ -24,9 +24,14 @@ public class LagerServerListener implements Listener {
         Player player = event.getPlayer();
         Block block = event.getBlockPlaced();
 
+        if (event.getItemInHand() == null ||
+                event.getItemInHand().getItemMeta() == null ||
+                event.getItemInHand().getItemMeta().getDisplayName() == null) {
+            return;
+        }
+
         // Prüfen, ob Spieler einen LagerServer platzieren will
         if (block.getType().name().equalsIgnoreCase("DISPENSER") &&
-                event.getItemInHand().getItemMeta() != null &&
                 event.getItemInHand().getItemMeta().getDisplayName().contains("§b§lLagerServer")) {
 
             boolean success = lagerServerBlock.placeLagerServer(player, block);
@@ -45,6 +50,7 @@ public class LagerServerListener implements Listener {
         Block block = event.getBlock();
 
         if (lagerServerBlock.isLagerServerBlock(block)) {
+
             boolean success = lagerServerBlock.removeLagerServer(block);
             if (!success) {
                 event.setCancelled(true);
