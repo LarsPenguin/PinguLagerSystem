986c5e2373c88ee30cbe29ea2879544189fbe705
LarsPenguin <grizzlybln123@gmail.com>
2025-12-06 19:22:12 +0100

    Refactor PinguLagerSystem initialization and handlers

--- src/main/java/de/pingu/lager/PinguLagerSystem.java
+++ src/main/java/de/pingu/lager/PinguLagerSystem.java
@@ -1,41 +1,56 @@
 package de.pingu.lager;
 
-import org.bukkit.plugin.java.JavaPlugin;
 import de.pingu.lager.api.VaultHandler;
-import de.pingu.lager.api.PlotSquaredHandler;
-import de.pingu.lager.listeners.PlayerListener;
-import de.pingu.lager.listeners.LagerServerListener;
+import de.pingu.lager.commands.LagerCommand;
 import de.pingu.lager.listeners.LagerKistenListener;
+import de.pingu.lager.listeners.LagerServerListener;
 import de.pingu.lager.storage.StorageManager;
+import de.pingu.lager.tasks.InputTask;
+import de.pingu.lager.tasks.OutputTask;
+import org.bukkit.plugin.java.JavaPlugin;
 
-public final class PinguLagerSystem extends JavaPlugin {
+public class PinguLagerSystem extends JavaPlugin {
 
-    private static PinguLagerSystem instance;
-    private StorageManager storageManager;
     private VaultHandler vaultHandler;
+    private StorageManager storageManager;
     private PlotSquaredHandler plotHandler;
+
+    public VaultHandler getVaultHandler() {
+        return vaultHandler;
+    }
+
+    public StorageManager getStorageManager() {
+        return storageManager;
+    }
+
+    public PlotSquaredHandler getPlotHandler() {
+        return plotHandler;
+    }
 
     @Override
     public void onEnable() {
-        instance = this;
-
         // Config laden
         saveDefaultConfig();
 
-        // Storage Manager initialisieren
-        storageManager = new StorageManager(this);
-
-        // Vault
+        // Handler initialisieren
         vaultHandler = new VaultHandler(this);
         vaultHandler.setupEconomy();
 
-        // PlotSquared
         plotHandler = new PlotSquaredHandler(this);
+        storageManager = new StorageManager(this);
 
         // Listener registrieren
-        getServer().getPluginManager().registerEvents(new PlayerListener(this), this);
         getServer().getPluginManager().registerEvents(new LagerServerListener(this), this);
         getServer().getPluginManager().registerEvents(new LagerKistenListener(this), this);
+
+        // Command registrieren
+        new LagerCommand(this);
+
+        // InputTask starten (asynchron)
+        new InputTask(this).start(getConfig().getLong("tasks.inputInterval", 20));
+
+        // OutputTask starten (asynchron)
+        new OutputTask(this).start(getConfig().getLong("tasks.outputInterval", 20));
 
         getLogger().info("PinguLagerSystem aktiviert!");
     }
@@ -44,18 +59,8 @@ public final class PinguLagerSystem extends JavaPlugin {
     public void onDisable() {
         getLogger().info("PinguLagerSystem deaktiviert!");
     }
+}
 
-    public static PinguLagerSystem getInstance() {
-        return instance;
-    }
-
-    public StorageManager getStorageManager() {
-        return storageManager;
-    }
-
-    public VaultHandler getVaultHandler() {
-        return vaultHandler;
-    }
 
     public PlotSquaredHandler getPlotHandler() {
         return plotHandler;
